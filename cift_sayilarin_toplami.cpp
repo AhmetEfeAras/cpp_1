0de6aa1fe20fc772235a61430f21c44b1a68d037
AhmetEfeAras <129206082+AhmetEfeAras@users.noreply.github.com>
2023-03-28 20:56:55 +0300

    update_2023.03.28_1
    burada iki adet dosya vardır.

---
+++ cift_sayilarin_toplami.cpp
@@ -0,0 +1,42 @@
+/*
+  1 den 100 e kadar olan sayýlardan çift olanlarýn toplamý
+*/
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int sayac = 1, toplam = 0;
+     int tek adet=0 ciftadet=0
+     int teklerintoplamý = 0, ciftlerinadeti = 0
+    while(sayac<10)
+    cout<<sayac<<".sayi giriniz."; cin>>sayi;
+    {
+
+
+
+        if(sayac%2 == 0)
+        {
+            // Cift sayilari gormek
+            cout<<sayac<<endl;
+            //cift sayilarin toplami
+            toplam = toplam + sayac;
+       ciftlerintoplamý = ciftlerintoplamý + sayi;
+       ciftadet = cidtadet + 1;
+        }
+        sayac = sayac+1;
+    cout<<"ciftlerinadedi;"
+
+    cout<<"ciftlerintoplamý;"
+
+    cout<<"tklerinadedi;"
+    cout<<"teklerintoplamý;"
+
+    }
+    cout<<"Toplam:"<<toplam<<endl;
+
+
+    return 0;
+
+ss
---
+++ parametre olarak 2. sayıyı ilkine üs olarak ekleme.c
@@ -0,0 +1,12 @@
+// num1 üs num2 olacak þekilde çalýþan metoýdu yazýnýz.
+#include <cmath>
+double calculateExponent(double num1, double num2) {
+	//yol 1
+	return	pow(num1, num2);
+
+	// yol2
+	//double sonuc = 1;
+	//for (int i = 1; i <= num2; i++)
+	//	sonuc = sonuc * num1;
+	//return sonuc;
+}
